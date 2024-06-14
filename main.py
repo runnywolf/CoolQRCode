69fc0733b31cdcb1b9cdfb9c3434107d732ba532
runnywolf <flowynnur@gmail.com>
2024-06-14 23:08:20 +0800

    v1

--- main.py
+++ main.py
@@ -30,29 +30,47 @@ def drawColorRect(draw: ImageDraw, pos: tuple[float, float], radius: float, colo
 	point2 = (pos[0]+radius, pos[1]+radius)
 	draw.rectangle([point1, point2], fill="#"+colorCode)
 
-def insertQRCodeToImg(inputImageUrl: str, outputImageUrl: str, s_data: str, correctLevel: int, radiusRate: float = 1) -> None: # 將字串轉為 QRCode 後
-	img_input = Image.open(inputImageUrl) # 讀取圖片
-	imageOriginalSize = img_input.size
-	img_input = img_input.resize((2048, 2048), Image.LANCZOS) # 等比例放大圖片
+def insertQRCodeToImg(
+	inputImageUrl: str, # 要美化的圖片的檔案路徑
+	outputImageUrl: str, #
+	s_data: str,
+	correctLevel: int,
+	finderPattern: str,
+	radiusRate: float = 1
+) -> None: # 將字串轉為 QRCode 後
+	img_input = Image.open(inputImageUrl).convert("RGBA") # 讀取圖片
+	imageOriginalSize = img_input.size # 保存圖片的原始大小
+	img_input = img_input.resize((2048, 2048), Image.LANCZOS) # 等比例放大圖片, 因為要使 QRCode 像素點平滑, 等等會縮小回原大小
 	
-	a_QRCode = getQRCodeArr(s_data, correctLevel)
-	QRCodeWidth = len(a_QRCode)
-	pixelWidth = img_input.size[0] / QRCodeWidth
+	img_overlay = Image.new("RGBA", img_input.size, (255, 255, 255, 0)) # 透明圖層, 用於繪製額外的東西在原始圖片上
+	draw = ImageDraw.Draw(img_overlay)
 	
-	draw = ImageDraw.Draw(img_input)
-	for i in range(QRCodeWidth):
+	a_QRCode = getQRCodeArr(s_data, correctLevel) # 取得 QRCode 矩陣
+	QRCodeWidth = len(a_QRCode) # QRCode 的像素邊長 (px)
+	pixelWidth = img_input.size[0] / QRCodeWidth # 每個 QRCode 像素對應到要美化的圖片上, 的大小
+	
+	for i in range(QRCodeWidth): # 繪製 QRCode 的每個點
 		for j in range(QRCodeWidth):
-			color = "000" if a_QRCode[j][i] else "fff"
-			if inRange(i, 1, QRCodeWidth-2) and inRange(j, 1, QRCodeWidth-2):
-				drawColorDot(draw, tupleMulti((0.5+i, 0.5+j), pixelWidth), pixelWidth/2 * radiusRate, color)
+			color = "000" if a_QRCode[j][i] else "fff" # QRCode 像素點是黑色還是白色
 			
 			if (i < 9 and j < 9) or (i >= QRCodeWidth-9 and j < 9) or (i < 9 and j >= QRCodeWidth-9) or\
 				 (inRange(i, QRCodeWidth-10, QRCodeWidth-6) and inRange(j, QRCodeWidth-10, QRCodeWidth-6)):
-				drawColorRect(draw, tupleMulti((0.5+i, 0.5+j), pixelWidth), pixelWidth/2, color)
+				if finderPattern == "rect": drawColorRect(draw, tupleMulti((0.5+i, 0.5+j), pixelWidth), pixelWidth/2, color) # 繪製定位點
+			elif inRange(i, 1, QRCodeWidth-2) and inRange(j, 1, QRCodeWidth-2):
+				drawColorDot(draw, tupleMulti((0.5+i, 0.5+j), pixelWidth), pixelWidth/2 * radiusRate, color) # 繪製資料點
 	
+	if finderPattern == "circle":
+		for pos in ((4.5, 4.5), (4.5, QRCodeWidth-4.5), (QRCodeWidth-4.5, 4.5)): # 繪製三個圓形的大定位點
+			for pixelRadius, color in ((4.5, "fff"), (3.5, "000"), (2.5, "fff"), (1.5, "000")): # 每個大定位點都有黑白相間的圓
+				drawColorDot(draw, tupleMulti(pos, pixelWidth), pixelWidth*pixelRadius, color+"b")
+		
+		for pixelRadius, color in ((2.5, "000"), (1.5, "fff"), (0.5, "000")): # 繪製右下角的小定位點
+			drawColorDot(draw, tupleMulti((QRCodeWidth-7.5, QRCodeWidth-7.5), pixelWidth), pixelWidth*pixelRadius, color+"4")
+	
+	img_input = Image.alpha_composite(img_input, img_overlay) # 將繪製的 QRCode 覆蓋到要美化的圖片上
 	img_input = img_input.resize(imageOriginalSize, Image.LANCZOS) # 等比例縮小圖片
-	img_input.save(outputImageUrl)
+	img_input.save(outputImageUrl) # 以給定路徑保存圖片
 
 #url = "https://www.youtube.com/@ShishiroBotan"
 url = "https://www.youtube.com/@bluenight1022"
-insertQRCodeToImg("night.png", "output.png", url, qrcode.ERROR_CORRECT_M, 0.13)
+insertQRCodeToImg("night.png", "output.png", url, qrcode.ERROR_CORRECT_M, "circle", 0.2)
